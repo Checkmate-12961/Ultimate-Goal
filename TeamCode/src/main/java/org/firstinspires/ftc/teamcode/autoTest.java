d8f6ffd25ab79150cf244d03cc0a102399548a4e
Andrew Craig <andrewdavidcraig@gmail.com>
2020-11-14 15:51:03 -0500

    Added new trajectory to give the human player a chance to correct goal position. Fixed trajectory sequencing. Added comments.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoTest.java
@@ -25,10 +25,14 @@ public class autoTest extends LinearOpMode {
                 .splineTo(new Vector2d(45,-36), Math.toRadians(180))
                 .build();
         Trajectory traj1 = drive.trajectoryBuilder(traj0.end())
-                .lineToSplineHeading(new Pose2d(64, -38, Math.toRadians(180)))
+                .lineToSplineHeading(new Pose2d(64, -36, Math.toRadians(180)))
                 .build();
-        //traj2 navigates the robot to the middle line
+        //traj2 pushes the robot slightly forward so the rings will be closer to the goal
         Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
+                .lineToSplineHeading(new Pose2d(64, -38 , Math.toRadians(180)))
+                .build();
+        //traj3 navigates the robot to the middle line
+        Trajectory traj3 = drive.trajectoryBuilder(traj2.end())
                 .splineTo(new Vector2d(10,-20),Math.toRadians(180))
                 .build();
 
@@ -36,14 +40,19 @@ public class autoTest extends LinearOpMode {
 
         if(isStopRequested()) return;
 
-        //Executes traj0 and traj1, positioning the bot before the goal
+        //Executes traj0 and traj1, positioning the bot before the goal. It frequently rams into it.
         drive.followTrajectory(traj0);
         drive.followTrajectory(traj1);
+        //waits for .75 seconds, so that the human player can push the goal back.
+        sleep(750);
+        //pushes bot forward, because the human player pushed the bot back, but the goal moved away.
+        drive.followTrajectory(traj2);
+
         //dumps rings into low goal
         drive.setIntakePowers(0, .5, .5);
         sleep(6000);
         drive.setIntakePowers(0,0,0);
-        //executes traj2, positioning the bot atop the middle line.
-        drive.followTrajectory(traj2);
+        //executes traj3, positioning the bot atop the middle line.
+        drive.followTrajectory(traj3);
     }
 }
