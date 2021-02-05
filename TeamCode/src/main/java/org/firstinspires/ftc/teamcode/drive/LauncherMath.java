bcb730ad9ae1923c2113cd9326d036a5e9f4fae7
FourInchKnife <riley.j.mclain@gmail.com>
2021-02-05 18:08:25 -0500

    got the numbers on point

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/LauncherMath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/LauncherMath.java
@@ -4,8 +4,8 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class LauncherMath {
-    public static double toX = -5;
-    public static double toY = -32;
-    public static double angle = -12;
+    public static double toX = -7;
+    public static double toY = -31;
+    public static double angle = -11;
     public static double power = 1;
 }
