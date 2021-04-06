f0b12d4ead94e5c233d3754b1c4f98150cd2fc44
FourInchKnife <riley.j.mclain@gmail.com>
2021-04-06 14:57:25 -0400

    started to split LauncherConstants

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/launcherConstants/EndgamePowerConstants.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.drive.launcherConstants;
+
+public class EndgamePowerConstants {
+    // power shot
+    public static double angle = 0; // angle the robot turns
+    public static double veloRight = 3500; // 0.72; //
+    public static double veloCenter = 3700; // 0.75; //
+    public static double veloLeft = 3800; // 0.72; //
+
+    // coordinates
+    public static double X = -10; //X coordinate for the rightmost powershot
+    public static double Y = -20;   //Y coordinate for the rightmost powershot
+    public static double pegDist = 7.7; // distance between each shot
+}
