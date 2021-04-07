9ce6c86d18f77b2ef27ffb38c6ac76c91aa6cc67
SimonVerrill <simon.a.verrill@gmail.com>
2021-04-07 15:04:19 -0400

    it no work and i dont know why

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/launcherConstants/AutoPowerConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/launcherConstants/AutoPowerConstants.java
@@ -7,11 +7,11 @@ public class AutoPowerConstants {
     // power shot
     public static double angle = 0; // angle the robot turns
     public static double veloRight = 3700; // 0.72; //
-    public static double veloCenter = 3700; // 0.75; //
+    public static double veloCenter = 3900; // 0.75; //
     public static double veloLeft = 3700; // 0.72; //
 
     // coordinates
     public static double X = -10;
-    public static double Y = -20;
-    public static double pegDist = 7.7; // distance between each shot
+    public static double Y = -18;
+    public static double pegDist = 11; // distance between each shot
 }
